cbffe62e6d37188e382c354069a5309b15341f97
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2023-08-04 19:34:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const openai = require('openai');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 
 app.use(bodyParser.json());
 
@@ -52,6 +52,42 @@ app.post('/debug', async (req, res) => {
     res.status(500).json({ error: 'Code debugging failed' });
   }
 });
+
+
+// Handle code quality assessment requests
+app.post('/assess-quality', async (req, res) => {
+  const { code } = req.body;
+
+  const prompt = `
+    Evaluate the quality of the following code:
+    Code Consistency: <Assess code for consistent style, naming conventions, and formatting>
+    Code Performance: <Assess code for efficient algorithms, optimized data structures, and overall performance>
+    Code Documentation: <Review code for comments, inline documentation, and clear explanations of logic>
+    Error Handling: <Examine code for proper error handling and graceful error recovery>
+    Code Testability: <Evaluate code for ease of testing, mocking, and overall testability>
+    Code Modularity: <Assess code for modular design, separation of concerns, and reusability>
+    Code Complexity: <Analyze code for complexity, convoluted logic, and potential code smells>
+    Code Duplication: <Identify code duplication and assess its impact on maintainability>
+    Code Readability: <Evaluate code for readability, clarity, and adherence to coding best practices>
+    Code:
+    ${code}
+  `;
+
+  try {
+    const response = await openai.Completion.create({
+      engine: 'davinci-codex',
+      prompt: prompt,
+      max_tokens: 150,
+    });
+
+    const qualityAssessment = response.choices[0].text.trim();
+    res.json({ qualityAssessment });
+  } catch (error) {
+    console.error('Code quality assessment failed:', error);
+    res.status(500).json({ error: 'Code quality assessment failed' });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
