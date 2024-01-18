852db5129c53e4971165160d84efcae537220813
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2024-01-18 11:02:36 +0530

    s

---
+++ sahil-app/src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+import Greating from "./components/greeting.js"
+
+
+
+function App() {
+  return (
+
+    <div className="App"> 
+
+      <Greating/>
+
+    </div>
+
+  
+  )
+ 
+  
+}
+
+export default App;
---
+++ sahil-app/src/components/greeting.js
@@ -0,0 +1,23 @@
+import styles from "./greeting.module.css"
+
+function Greating(){
+    console.log(styles.App)
+    return (
+        <>
+            
+        <h1>
+                greeting business
+
+                <p className="App">
+                    The paragraph is working fine and the work is going well
+
+                </p>
+
+        </h1>
+        
+        </>
+    )
+    
+}
+
+export default Greating;
